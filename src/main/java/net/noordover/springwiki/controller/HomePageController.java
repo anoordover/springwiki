c7ac6942ab0fa141e3d32bd010e82821a8864f69
Arno Noordover <anoordover@users.noreply.github.com>
2018-11-02 17:35:44 +0100

    sonar controle

--- src/main/java/net/noordover/springwiki/controller/HomePageController.java
+++ src/main/java/net/noordover/springwiki/controller/HomePageController.java
@@ -6,9 +6,9 @@ import net.noordover.springwiki.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,32 +16,32 @@ public class HomePageController {
         @Autowired
         private ArticleRepository articleRepository;
 
-        @RequestMapping(path="/", method = RequestMethod.GET)
+        @GetMapping(path="/")
         public ModelAndView getHomePage() {
                 ModelAndView result = new ModelAndView("details");
                 result.addObject("article", articleRepository.findByTopic("HomePage"));
                 return result;
         }
 
-        @RequestMapping(path = "/articles/add", method = RequestMethod.GET)
+        @GetMapping(path = "/articles/add")
         public String createProduct(Model model) {
                 model.addAttribute("article", new Article());
                 return "edit";
         }
 
-        @RequestMapping(path = "/articles/{topic}", method = RequestMethod.GET)
+        @GetMapping(path = "/articles/{topic}")
         public String createProduct(Model model, @PathVariable String topic) {
                 model.addAttribute("article", articleRepository.findByTopic(topic));
                 return "details";
         }
 
-        @RequestMapping(path = "/articles/edit/{topic}", method = RequestMethod.GET)
+        @GetMapping(path = "/articles/edit/{topic}")
         public String editArticle(Model model, @PathVariable String topic) {
                 model.addAttribute("article", articleRepository.findByTopic(topic));
                 return "edit";
         }
 
-        @RequestMapping(path = "articles", method = RequestMethod.POST)
+        @PostMapping(path = "articles")
         @Transactional
         public String saveProduct(Article article) {
                 articleRepository.save(article);
